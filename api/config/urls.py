2e5d7f0cd3368ef2b19eff234338c74c4efaab5d
Martyna Witkowska <mar.witkowska@gmail.com>
2023-09-27 16:35:02 +0200

    spcetacular for generating api schema

--- api/config/urls.py
+++ api/config/urls.py
@@ -18,9 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import django_eventstream
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("events/", include(django_eventstream.urls), {'channels': ['test']}),
-    path('api/v1/', include("signal_server.urls"))
+    path('api/v1/', include("signal_server.urls")),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
