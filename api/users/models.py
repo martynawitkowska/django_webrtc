74eb422a1fae51e98d6532c7ca5c81242a99e422
Martyna Witkowska <mar.witkowska@gmail.com>
2023-09-29 15:27:37 +0200

    avatars for users

--- api/users/models.py
+++ api/users/models.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+from django.db import models
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Avatar(models.Model):
+    avatar = models.FileField(upload_to="avatars/")
+    owner = models.ForeignKey(get_user_model(), related_name="avatars", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Avatar for {self.owner.username}"
