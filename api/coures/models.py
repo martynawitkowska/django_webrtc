4f0df13b4b5a0b53d9102325b9125e8090653ca7
Martyna Witkowska <mar.witkowska@gmail.com>
2023-09-29 22:11:34 +0200

    basic admin models setup, basic serializers

--- api/coures/models.py
+++ api/coures/models.py
@@ -18,6 +18,10 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
